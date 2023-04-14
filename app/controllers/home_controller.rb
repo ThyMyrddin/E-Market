92937f043b245ef75a455a4a866262748f6c075a
Merizak Houssam <merizakhsm@gmail.com>
2023-04-14 14:51:45 +0000

    added sorting and filtering

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @articles = Article.all
+    @q = Article.ransack(params[:q])
+    @articles = @q.result(distinct: true)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
     has_many_attached :pictures
     belongs_to :user
+    def self.ransackable_attributes(auth_object = nil)
+        ["category", "updated_at","label", "localisation", "price"]
+      end
 end
