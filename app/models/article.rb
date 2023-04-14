3936d5a66597baca56c1f1811e444c91362c2d3f
Merizak Houssam <merizakhsm@gmail.com>
2023-04-14 16:24:28 +0000

    some validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ApplicationRecord
     has_many_attached :pictures
     belongs_to :user
     def self.ransackable_attributes(auth_object = nil)
-        ["category", "updated_at","label", "localisation", "price"]
+        ["category", "created_at","label", "localisation", "price"]
       end
 end
