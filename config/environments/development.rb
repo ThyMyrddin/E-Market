f55faec1132cec98d9cf76cbb9a331a7a96a38e4
Houssam Merizak <merizakhsm@gmail.com>
2023-05-02 17:04:07 +0100

    mailer set

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,5 +68,14 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => '4c3fa0aa2352e7',
+    :password => 'fc13ec7b3396ed',
+    :address => 'sandbox.smtp.mailtrap.io',
+    :domain => 'sandbox.smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+}
 
 end
